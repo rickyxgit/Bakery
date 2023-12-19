1f250401c2b3e46783984ff1a054a971ad54dc92
Ricky Xian <84099067+rickyxgit@users.noreply.github.com>
2023-12-19 15:33:32 -0800

    about ~75% done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import chef from "../assets/chef.jpg";
+
 import "../styles/about.css";
 
 function About() {
   return (
     <div className="about">
-      <div class="flex-container">
+      <div className="flex-container">
 
-        <div class="flex-item">
-          <div class="inner">
-
-          
+        <div className="flex-item">
+          <div className="inner">
             <h2> Our Story: </h2> 
             <p>
               At Sweet Delights, we believe in the magic of pastry-making. 
@@ -21,22 +19,30 @@ function About() {
           </div>
         </div>
 
-        <div class="flex-item">
-          <h2> Crafting Sweet Memories, One Bite at a Time </h2>
-          <p>
-            Our talented team of pastry artisans brings passion and creativity to every confection we create. 
-            From delicate pastries to artisanal cakes, each treat is a masterpiece, meticulously crafted with the finest ingredients. 
-            We believe that a good pastry not only tantalizes the taste buds but also captures the essence of the moment.
-          </p>
+        <div className="flex-item">
+          <div className="inner2">
+            <div className="left"></div>
+            <div className="right">
+              <h2> Crafting Sweet Memories, One Bite at a Time </h2>
+              <p>
+                Our talented team of pastry artisans brings passion and creativity to every confection we create. 
+                From delicate pastries to artisanal cakes, each treat is a masterpiece, meticulously crafted with the finest ingredients. 
+                We believe that a good pastry not only tantalizes the taste buds but also captures the essence of the moment.
+              </p>
+              </div>
+          </div>
         </div>
 
-        <div class="flex-item">
-          <h2> Quality Ingredients, Artful Creations </h2>
-          <p>
-            Quality is at the heart of everything we do. We source only the finest ingredients, 
-            ensuring that every bite is a symphony of flavors. Our commitment to excellence extends 
-            beyond taste – our pastries are crafted with precision and artistry. Each creation tells a story of dedication to the craft and a love for the sweet life.
-          </p>
+        <div className="flex-item">
+          <div className="inner3">
+            <h2> Quality Ingredients, Artful Creations </h2>
+            <p>
+              Quality is at the heart of everything we do. We source only the finest ingredients, 
+              ensuring that every bite is a symphony of flavors. Our commitment to excellence extends 
+              beyond taste – our pastries are crafted with precision and artistry. Each creation tells a story of dedication to the craft and a love for the sweet life.
+            </p>
+          </div>
+          
           
 
       </div>
