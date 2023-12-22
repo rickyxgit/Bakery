879c4d7ad6ac88ea27be591f18ed617d028e053b
Ricky Xian <84099067+rickyxgit@users.noreply.github.com>
2023-12-21 18:21:31 -0800

    Navbar CSS mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,93 @@
-import React, { useState } from 'react'
-import {Link, NavLink} from 'react-router-dom'
-import '../styles/Navbar.css';
-import ReorderIcon from '@mui/icons-material/Reorder';
+import React, { useState, useEffect, useRef } from "react";
+import { NavLink } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@mui/icons-material/Reorder";
 
 function Navbar() {
-    const [openLinks, setOpenLinks] = useState(false); 
-    const toggleNavbar = () => {
-      setOpenLinks(!openLinks);
+
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        // Click occurred outside the dropdown, close it
+        setShowDropdown(false);
+      }
     };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+
+
+  const toggleDropdown = (event) => {
+    event.stopPropagation(); 
+    setShowDropdown(!showDropdown);
+    console.log("CLICK ON");
+  };
+
+  const closeDropdown = () => {
+    setShowDropdown(false);
+  };
+
+  
+ 
+ 
 
   return (
     <div className="navbar">
-      <div className="leftSide" id={openLinks ? "open" : "close"}>
+      <div className="leftSide">
         <h1> Anne's Bakery </h1>
-        <div className="hiddenLinks">
-          <NavLink to="/" > Home </NavLink>
-          <Link to="/menu"> Menu </Link>
-          <Link to="/about"> About </Link>
-          <Link to="/contact"> Contact</Link>
-        </div>
       </div>
       <div className="rightSide">
-        <NavLink to="/" activeclassname="active"> Home </NavLink>
-        <NavLink to="/menu" activeclassname="active"> Menu </NavLink>
-        <NavLink to="/about" activeclassname="active"> About </NavLink>
-        <NavLink to="/contact" activeclassname="active"> Contact</NavLink>
-        <button onClick={toggleNavbar}>
-          <ReorderIcon/>
+        <NavLink to="/" activeclassname="active" className="desktopAnchor">
+          {" "}
+          Home{" "}
+        </NavLink>
+        <NavLink to="/menu" activeclassname="active" className="desktopAnchor">
+          {" "}
+          Menu{" "}
+        </NavLink>
+        <NavLink to="/about" activeclassname="active" className="desktopAnchor">
+          {" "}
+          About{" "}
+        </NavLink>
+        <NavLink to="/contact" activeclassname="active" className="desktopAnchor">
+          {" "}
+          Contact
+        </NavLink>
+       
+        <button onClick={toggleDropdown}>
+          <ReorderIcon />
         </button>
+
+        {/* Dropdown Menu */}
+        {showDropdown && (
+          <div className="dropdown" ref={dropdownRef}>
+            <NavLink to="/" activeclassname="active" className="dropdownAnchor" onClick={closeDropdown}>
+              Home
+            </NavLink>
+            <NavLink to="/menu" activeclassname="active" className="dropdownAnchor" onClick={closeDropdown}>
+              Menu
+            </NavLink>
+            <NavLink to="/about" activeclassname="active" className="dropdownAnchor" onClick={closeDropdown}>
+              About
+            </NavLink>
+            <NavLink to="/contact" activeclassname="active" className="dropdownAnchor" onClick={closeDropdown}>
+              Contact
+            </NavLink>
+          </div>
+        )}
+
+       
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
